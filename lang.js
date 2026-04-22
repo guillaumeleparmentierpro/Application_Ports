38432bd173480a1db90288d514b8cafd9abf2006
guillaumeleparmentierpro <guillaume.leparmentier.pro@gmail.com>
2026-04-22 18:27:05 +0200

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,24 @@
+const translations = {
+    fr: {
+        label_gate: "PORTE",
+        status_open: "OUVERTE",
+        status_closed: "FERMÉE",
+        next_open: "PROCHAINE OUVERTURE",
+        next_close: "PROCHAINE FERMETURE",
+        btn_planning: "📅 PLANNING COMPLET",
+        btn_security: "🆘 SÉCURITÉ ET RAPPEL",
+        low_tide: "BASSE MER",
+        high_tide: "PLEINE MER"
+    },
+    en: {
+        label_gate: "GATE",
+        status_open: "OPEN",
+        status_closed: "CLOSED",
+        next_open: "NEXT OPENING",
+        next_close: "NEXT CLOSING",
+        btn_planning: "📅 FULL SCHEDULE",
+        btn_security: "🆘 SAFETY & RULES",
+        low_tide: "LOW TIDE",
+        high_tide: "HIGH TIDE"
+    }
+};
