ad58cf843a4274ddbfad4ec4a7910632658cda05
guillaumeleparmentierpro <guillaume.leparmentier.pro@gmail.com>
2026-04-22 19:23:34 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -5,7 +5,7 @@ const translations = {
         status_closed: "FERMÉE",
         next_open: "PROCHAINE OUVERTURE",
         next_close: "PROCHAINE FERMETURE",
-        btn_planning: "📅 PLANNING COMPLET",
+        btn_planning: "HORAIRES & MARÉES",
         btn_security: "🆘 SÉCURITÉ ET RAPPEL",
         low_tide: "BASSE MER",
         high_tide: "PLEINE MER"
@@ -16,7 +16,7 @@ const translations = {
         status_closed: "CLOSED",
         next_open: "NEXT OPENING",
         next_close: "NEXT CLOSING",
-        btn_planning: "📅 FULL SCHEDULE",
+        btn_planning: "FULL SCHEDULE",
         btn_security: "🆘 SAFETY & RULES",
         low_tide: "LOW TIDE",
         high_tide: "HIGH TIDE"
