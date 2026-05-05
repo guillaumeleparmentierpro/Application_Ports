0f64a72f82191ab91f2c31d70b330e4242790ad9
guillaumeleparmentierpro <guillaume.leparmentier.pro@gmail.com>
2026-05-05 13:58:48 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -3,6 +3,8 @@ const translations = {
         // --- COMMUN ---
         btn_back_arrow: "◀ RETOUR",
 // --- NOUVEAUTÉS PAGE PARTAGE ---
+        currents_title: "Courants marins",
+    currents_hint: "👆 Touchez la carte pour explorer la zone.",
         planning_warning_text: "<strong>* Attention :</strong> Les horaires peuvent varier selon météo et marée (+/- 30 min). Chenalage à prévoir inférieur à 10 minutes. Toutes les données du site sont à titre indicatif. L'éditeur décline toute responsabilité en cas d'erreur. Le capitaine est responsable de sa navigation.",
     btn_understood: "J'AI COMPRIS",
         desc_century21: "Agence immobilière.",
@@ -188,6 +190,8 @@ disclaimer_partners: "<strong style=\"color:#b03a2e;\">*Attention :</strong> L'�
         // --- COMMUN ---
         btn_back_arrow: "◀ BACK",
 // --- NOUVEAUTÉS PAGE PARTAGE ---
+        currents_title: "Ocean Currents",
+    currents_hint: "👆 Touch the map to explore the area.",
         planning_warning_text: "<strong>* Warning :</strong> Times may vary depending on weather and tide (+/- 30 min). Channeling to be expected under 10 minutes. All data on the site is indicative. The publisher declines all responsibility in the event of an error. The captain is responsible for their navigation.",
     btn_understood: "I UNDERSTAND",
         desc_century21: "Real estate agency.",
